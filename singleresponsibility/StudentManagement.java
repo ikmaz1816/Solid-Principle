9c2056c682b3d767ad9c21548e2d18065b9a40ed
ikmaz1816 <ikmaz1816@gmail.com>
2023-11-04 22:17:33 +0530

    S for Single Responsibility

---
+++ singleresponsibility/StudentManagement.java
@@ -0,0 +1,31 @@
+package singleresponsibility;
+
+
+public class StudentManagement {
+	 
+	/*
+	  Higher level module should not have 
+	 */
+	private Student s;
+	
+	public StudentManagement(Student s)
+	{
+	}
+	
+	public void addStudent(Student s)
+	{
+		
+	}
+	public void updateStudent(Student s)
+	{
+		
+	}
+	public void deleteStudent(int id)
+	{
+		
+	}
+	public Student getDetails()
+	{
+		return s;
+	}
+}
